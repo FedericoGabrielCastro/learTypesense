d4450653e3067ba483e42d4c8d7365b4395cbce0
federico <fgc_castro@live.com>
2022-11-29 15:18:54 -0300

    [ITEMS]
    You can choose the number of items that appear

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Pagination } from 'react-instantsearch-dom'
+import { HitsPerPage, Pagination } from 'react-instantsearch-dom'
 import BusinessContent from '../components/BusinessComponents/BusinessContent'
 
 const HomePage = () => {
     return (
         <div className='flex pt-5 flex-col items-center justify-center w-screen'>
             <p className=' text-gray-400 p-5'>Here below the ranking with the largest companies in the world, by their market capitalization on May 17, 2022, in billions of dollars:</p>
+            <HitsPerPage
+                items={[
+                    {label: '3 per page', value: 3},
+                    {label: '9 per page', value: 9},
+                    {label: '18 per page', value: 18}
+                ]}
+                defaultRefinement={9}
+            />
             <div className='pb-5 pt-5'  >
                 <Pagination className="scale-75 lg:scale-100"/>
             </div>
